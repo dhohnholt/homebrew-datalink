2cff13a14e5ab2a940738736ae4d1c9a9c985d3c
David Hohnholt <dhohnholt@gmail.com>
2026-09-17 07:30:10 -0600

    datalink-scanner 1.7.5 bottle

--- Formula/datalink-scanner.rb
+++ Formula/datalink-scanner.rb
@@ -8,6 +8,11 @@ class DatalinkScanner < Formula
   license "MIT"
   head "https://github.com/dhohnholt/datalink_Mac_OS_interface.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/dhohnholt/datalink_Mac_OS_interface/releases/download/v1.7.5"
+    sha256 cellar: :any, arm64_tahoe: "a6ff23580d511930a5718fab7a8b91de8345181fed581b182c05609a47239310"
+  end
+
 
 
 
