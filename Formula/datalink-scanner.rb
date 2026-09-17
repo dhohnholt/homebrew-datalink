6d16de6024d3fc9e932bbbe41849c2264593998d
David Hohnholt <dhohnholt@gmail.com>
2026-09-17 05:07:59 -0600

    datalink-scanner 1.6.0

--- Formula/datalink-scanner.rb
+++ Formula/datalink-scanner.rb
@@ -3,8 +3,8 @@ class DatalinkScanner < Formula
 
   desc "macOS interface for the Apperson DataLink 1200 optical mark scanner"
   homepage "https://github.com/dhohnholt/datalink_Mac_OS_interface"
-  url "https://github.com/dhohnholt/datalink_Mac_OS_interface/archive/refs/tags/v1.5.0.tar.gz"
-  sha256 "91f30466af86f97857171169aa2efa09128f87779fde0f9c15f7c6f5ef3c5278"
+  url "https://github.com/dhohnholt/datalink_Mac_OS_interface/archive/refs/tags/v1.6.0.tar.gz"
+  sha256 "3872d194251b5df04e2715ba91a883e4cd60265ade3ce91ceb72f11d19b432ee"
   license "MIT"
   head "https://github.com/dhohnholt/datalink_Mac_OS_interface.git", branch: "main"
 
